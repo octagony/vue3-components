bdff06ef6e3d9b22224278f47b88a102d0928bdf
octagony <moctagony@gmail.com>
2024-01-20 23:39:46 +0300

    [FEAT] Setup checkbox component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import Tooltip from './components/Tooltip/Tooltip.vue'
+import { ref } from 'vue'
+import Checkbox from './components/Checkbox/Checkbox.vue'
+
+const check = ref(true);
 </script>
 
 <template>
   <div class="container">
-    <Tooltip position="top" :timer="2000">
-      <template #tooltip-inner>Click me</template>
-      <template #tooltip-content>Успешно скопировано!</template>
-    </Tooltip>
+    <Checkbox v-model="check" />
+          {{check}}
   </div>
 </template>
 
