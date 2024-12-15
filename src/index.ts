57ec6f1a93a491e7df41bfa78616353fb550ceb8
octagony <moctagony@gmail.com>
2024-12-15 19:29:15 +0300

    [FEAT] Publish to npm

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Button from './components/Button/Button.vue'
+import Checkbox from './components/Checkbox/Checkbox.vue'
+import Tabs from './components/Tabs/Tabs.vue'
+import Tooltip from './components/Tooltip/Tooltip.vue'
+
+export { Button, Checkbox, Tabs, Tooltip }
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), dts()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-3-simple-component',
+      fileName: 'vue-3-simple-component'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
